7fa41ab6a0691453dc208e7606229281eef4d2d8
chayaWakshtok <38976767+chayaWakshtok@users.noreply.github.com>
2024-06-11 18:46:04 +0300

    likes

--- angular/src/app/models/like.ts
+++ angular/src/app/models/like.ts
@@ -1,8 +1,8 @@
 export class Like {
-  id: number;
+  id: number | undefined;
   userId: number;
   recipeId: number;
-  constructor(id: number, userId: number, recipeId: number) {
+  constructor(userId: number, recipeId: number, id?: number) {
     this.id = id;
     this.userId = userId;
     this.recipeId = recipeId;
--- angular/src/app/recipe-detail/recipe-detail.component.ts
+++ angular/src/app/recipe-detail/recipe-detail.component.ts
@@ -1,10 +1,10 @@
 import { ViewportScroller } from '@angular/common';
 import { Component, ElementRef, ViewChild } from '@angular/core';
-import jsPDF from 'jspdf';
 import { Recipe } from '../models/recipe';
 import { RecipeService } from '../services/recipe.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { StorageService } from '../services/storage.service';
+import { Like } from '../models/like';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -16,23 +16,28 @@ export class RecipeDetailComponent {
   recipe!: Recipe;
   recipesCategory: Recipe[] = [];
   isLike: boolean = false;
+  isLogin: boolean = false;
 
   constructor(private viewportScroller: ViewportScroller,
     public recipeService: RecipeService,
     private activatedRoute: ActivatedRoute,
-    private storageService: StorageService
+    private storageService: StorageService,
+    private router: Router
   ) { }
 
   ngOnInit(): void {
     const id = Number(this.activatedRoute.snapshot.paramMap.get('id'));
+    this.isLogin = this.storageService.isLoggedIn();
     if (id) {
       this.recipeService.getRecipe(id).subscribe(res => {
         this.recipe = res;
-        var id = this.storageService.getUser().id;
-        this.recipe.likes
-          ? (this.isLike =
-            this.recipe.likes.findIndex((x) => x.userId == id) >= 0)
-          : (this.isLike = false);
+        if (this.isLogin) {
+          var id = this.storageService.getUser().id;
+          this.recipe.likes
+            ? (this.isLike =
+              this.recipe.likes.findIndex((x) => x.userId == id) >= 0)
+            : (this.isLike = false);
+        }
         this.recipeService.getRecipesByCategory(this.recipe.categoryId ?? 0).subscribe(res => {
           this.recipesCategory = res;
         })
@@ -45,17 +50,21 @@ export class RecipeDetailComponent {
     this.viewportScroller.scrollToAnchor("share-item");
   }
 
-  //   comment_id: 87
-  // reply_user: 1
-  // reply_text: nnnnn
-  // reply_posted: 1
-  //type: reply
 
-  //   comment_item: 1
-  // comment_user: 1
-  // comment_text: sdasdas
-  // comment_posted: 1
-  //type:comment
+  removeLike() {
+    var id = this.storageService.getUser().id;
+    var like = this.recipe.likes?.find((x) => x.userId == id)
 
+  }
 
+  addLike() {
+    if (!this.isLogin) {
+      this.router.navigate(['/auth/signin']);
+    }
+    else{
+      var like = new Like(this.storageService.getUser().id, this.recipe.id);
+      
+
+    }
+  }
 }
